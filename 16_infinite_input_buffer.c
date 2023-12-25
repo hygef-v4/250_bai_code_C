fc447c09ce391208e66d477ae53b663d61ca2df7
Charles <138696615+hungsct1702@users.noreply.github.com>
2023-12-25 15:15:53 +0700

    Add files via upload

---
+++ 16_infinite_input_buffer.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define BUFFER_SIZE 5
+
+int main(void)
+{
+    /*int buffer[BUFFER_SIZE];
+
+    for (int i = 0; i < BUFFER_SIZE; i++)
+    {
+        printf("Enter: ");
+        scanf("%d", &buffer[i]);
+    }
+    int total = 0;
+    for (int i = 0; i < BUFFER_SIZE; i ++)
+    {
+        printf("buffer[%d] = %d\n", i, buffer[i]);
+        total += buffer[i];
+    }
+    printf("Total: %d", total);
+    */
+    
+    return 0;
+}
