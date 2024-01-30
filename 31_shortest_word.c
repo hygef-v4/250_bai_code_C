00cd7072752825d29f55bac0701679d541b633df
Charles <138696615+hungsct1702@users.noreply.github.com>
2024-01-30 21:44:31 +0700

    Add files via upload

---
+++ 31_shortest_word.c
@@ -0,0 +1,47 @@
+//Program: Find length of the shortest word 
+
+#include <stdio.h>
+#include <string.h>
+
+int largest_word(char *s);
+
+int main()
+{
+  // test out the function with a test string
+  char s[] = "This is, theres way Again...";
+  int largest_length = largest_word(s);
+  printf("%d\n", largest_length);
+
+  return 0;
+}
+
+int largest_word(char *s)
+{
+    int min = 99999; 
+    int len = strlen(s);
+    int count = 0; 
+    char non_words[] = " ,;.\n\t";
+
+    int i = 0;
+    
+    while (i < len)  
+    {
+        while (i < len && strchr(non_words, s[i]) == NULL) // count char in a word
+        {   
+            i ++;
+            count ++;
+        }
+    
+        if (count < min ) min = count;
+    
+        while (i < len && strchr(non_words, s[i]) != NULL) // skip non-word characters
+        {
+            i ++;
+        }
+    
+        count = 0; // reset the count
+    }
+
+    return min;
+}
+
