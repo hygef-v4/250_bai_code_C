0b14b67b9d7bc63ce6a7c73be041d0c40927c84b
hungsct1702 <hungsct1702@gmail.com>
2024-03-04 22:40:38 +0700

    up

--- 42_remove_extra_space.c
+++ 42_remove_extra_space.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-//       Easy     Code - EzC  sdsd
+
 // if s[0] or s[n]= " " --> delete " "
 int main()
 {
@@ -8,7 +8,7 @@ int main()
   char s[100];
   printf("Input a string: ");
   scanf("%[^\n]s", s);
-
+  // Easy Code - EzC s 
   for(i=0; i < strlen(s); i++)
   {
     if(s[i] == ' ')
@@ -19,7 +19,16 @@ int main()
     }
 
   }
-  printf("%s", s);
+  // remove leading space and ending space
+  if(s[0] == ' ')
+  {
+    strcpy(&s[0], &s[1]);
+  }
 
+  printf("%s", s);
+  if(s[i-1] == ' ')
+  {
+    s[i-1] = '\0';
+  }
   return 0;
 }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,1 @@
+       Easy     Code - EzC s 
